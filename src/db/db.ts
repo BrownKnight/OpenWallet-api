73e9f10c5aae7c4e8b6e59ee6e59b488cc00a9f0
BrownKnight <amanrk@hotmail.co.uk>
2020-11-14 18:06:37 +0000

    Setup basic institution entity

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,6 @@
 import pkg from "typeorm";
 const { createConnection } = pkg;
-import { Insitution } from "./entity/Institution.js";
+import { Institution } from "./entity/Institution.js";
 
 export async function initDb(): Promise<void> {
   console.log(
@@ -13,18 +13,18 @@ export async function initDb(): Promise<void> {
     username: process.env.OW_DATABASE_USERNAME,
     password: process.env.OW_DATABASE_PASSWORD,
     database: process.env.OW_DATABASE_NAME,
-    entities: [Insitution],
+    entities: [Institution],
     synchronize: true,
     //logging: true,
   })
     .then(() => {
       console.log(
-        `Conected to db at ${process.env.OW_DATABASE_HOST}:${process.env.OW_DATABASE_PORT}`
+        `Conected to db ${process.env.OW_DATABASE_NAME} at ${process.env.OW_DATABASE_HOST}:${process.env.OW_DATABASE_PORT}`
       );
     })
     .catch((err) => {
       console.error(
-        `Could not connect to db at ${process.env.OW_DATABASE_HOST}:${process.env.OW_DATABASE_PORT}`
+        `Could not connect to db ${process.env.OW_DATABASE_NAME} at ${process.env.OW_DATABASE_HOST}:${process.env.OW_DATABASE_PORT}`
       );
       console.error(err);
     });
--- src/db/entity/Institution.ts
+++ src/db/entity/Institution.ts
@@ -1,24 +1,20 @@
 import pkg from "typeorm";
 import { CredentialsType } from "./enum/CredentialsType.js";
+import { DataSource } from "./enum/DataSource.js";
 import { BaseEntity } from "./OWEntity.js";
-const { Column } = pkg;
+const { Column, Entity } = pkg;
 
-export class Insitution extends BaseEntity {
+@Entity()
+export class Institution extends BaseEntity {
   @Column({ type: "character varying" })
   external_id?: string;
 
   @Column({ type: "character varying" })
   fullName?: string;
 
-  @Column({ type: "character varying", enum: CredentialsType })
+  @Column({ type: "enum", enum: CredentialsType })
   credentialsType?: CredentialsType;
 
-  // @Column({ type: "character varying" })
-  // fullName?: string;
-
-  // @Column({ type: "character varying" })
-  // fullName?: string;
-
-  // @Column({ type: "character varying" })
-  // fullName?: string;
+  @Column({ type: "enum", enum: DataSource })
+  dataSource?: DataSource;
 }

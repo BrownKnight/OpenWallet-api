924b3d5dd652e02f399e02e2bfbc7fc2147a7df1
BrownKnight <amanrk@hotmail.co.uk>
2020-12-03 19:34:17 +0000

    Remove password field from fetch methods

--- src/service/UserLoginService.ts
+++ src/service/UserLoginService.ts
@@ -1,9 +1,30 @@
 import { UserLoginDAO } from "@db/DAO/UserLoginDAO";
 import { UserLogin } from "@db/entity/UserLogin";
 import { BaseEntityService } from "@service/BaseEntityService";
+import { ServiceResponse } from "./ServiceResponse";
 
 export class UserLoginService extends BaseEntityService<UserLogin> {
   constructor() {
     super(UserLoginDAO);
+  }
+
+  // Override getAll here to remove the password field
+  async getAll(): Promise<ServiceResponse<UserLogin>> {
+    const response = await super.getAll();
+    response.entities.forEach((entity) => {
+      entity.password = "";
+    });
+
+    return response;
+  }
+
+  // Override getById here to remove the password field
+  async getById(id: number): Promise<ServiceResponse<UserLogin>> {
+    const response = await super.getById(id);
+    response.entities.forEach((entity) => {
+      entity.password = "";
+    });
+
+    return response;
   }
 }

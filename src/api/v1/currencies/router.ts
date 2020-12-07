74e69c5e5587b2e7363b8810b492b198d5810a35
BrownKnight <amanrk@hotmail.co.uk>
2020-12-07 21:34:08 +0000

    Make currecny router CUD operations admin only

--- src/api/v1/currencies/router.ts
+++ src/api/v1/currencies/router.ts
@@ -1,6 +1,8 @@
 import { EntityRouter } from "@api/EntityRouter";
+import { AdminOnly } from "@api/middleware/adminOnlyDecorator";
 import { Currency } from "@db/entity/Currency";
 import { CurrencyService } from "@service/CurrencyService";
+import { Request, Response } from "express";
 
 export class CurrenciesRouter extends EntityRouter<Currency, CurrencyService> {
   constructor() {
@@ -9,5 +11,15 @@ export class CurrenciesRouter extends EntityRouter<Currency, CurrencyService> {
 
   initRoutes(): void {
     // No extra routes
+  }
+
+  @AdminOnly()
+  protected async save(req: Request, res: Response): Promise<void> {
+    super.save(req, res);
+  }
+
+  @AdminOnly()
+  protected async deleteById(req: Request, res: Response): Promise<void> {
+    super.save(req, res);
   }
 }
